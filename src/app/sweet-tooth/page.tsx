63ea1b8c590937324ec5576097619227ea13c190
dolodorsey <dolodorsey@gmail.com>
2026-08-06 05:57:46 -0400

    Activate complete Sweet Tooth customer website

---
+++ src/app/sweet-tooth/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from 'next';
+import CasperCommercePage from '@/components/casper-commerce/CasperCommercePage';
+import { remainingCasperBrands } from '@/lib/casper-commerce-config';
+import { buildCasperCommerceMetadata } from '@/lib/casper-commerce-metadata';
+
+const brand = remainingCasperBrands['sweet-tooth'];
+export const metadata: Metadata = buildCasperCommerceMetadata(brand);
+
+export default function SweetToothPage() {
+  return <CasperCommercePage brand={brand} />;
+}
